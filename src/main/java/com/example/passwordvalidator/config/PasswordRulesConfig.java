774bcd441d7c1cc2dd980572120cdf0f48de1470
paulaallemand <paulaisdev@gmail.com>
2025-01-13 14:58:15 -0300

    Add password rules config file

---
+++ src/main/java/com/example/passwordvalidator/config/PasswordRulesConfig.java
@@ -0,0 +1,30 @@
+package com.example.passwordvalidator.config;
+
+import com.example.passwordvalidator.domain.PasswordRule;
+import com.example.passwordvalidator.domain.rules.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+public class PasswordRulesConfig {
+
+    @Bean
+    public List<PasswordRule> passwordRules() {
+        return List.of(
+                new MinLengthRule(9),
+                new UpperCaseRule(),
+                new SpecialCharRule(Set.of('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+')),
+                new UniqueCharacterRule(),
+                new ValidCharacterRule(Set.of('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+',
+                        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'))
+        );
+    }
+
+}
