7e2e2d05242e3e93457d454cc6bd23ce203e3212
paulaallemand <paulaisdev@gmail.com>
2025-01-13 15:00:20 -0300

    Add firts tests :eyes:

---
+++ src/test/java/com/example/passwordvalidator/rules/SpecialCharRuleTest.java
@@ -0,0 +1,24 @@
+package com.example.passwordvalidator.rules;
+
+import com.example.passwordvalidator.domain.rules.SpecialCharRule;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SpecialCharRuleTest {
+
+    private final SpecialCharRule rule = new SpecialCharRule(Set.of('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+'));
+
+    @Test
+    void testPasswordWithSpecialCharacter() {
+        assertTrue(rule.validate("AbTp9!fok"));
+    }
+
+    @Test
+    void testPasswordWithoutSpecialCharacter() {
+        assertFalse(rule.validate("AbTp9fok"));
+    }
+}
