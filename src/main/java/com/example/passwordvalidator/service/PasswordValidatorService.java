5430a89bc766066c2a8979b1ed5876e2102a689a
paulaallemand <paulaisdev@gmail.com>
2025-01-13 14:58:22 -0300

    Add password service file

---
+++ src/main/java/com/example/passwordvalidator/service/PasswordValidatorService.java
@@ -0,0 +1,28 @@
+package com.example.passwordvalidator.service;
+
+import com.example.passwordvalidator.domain.PasswordRule;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PasswordValidatorService {
+
+    private final List<PasswordRule> rules;
+
+    public PasswordValidatorService(List<PasswordRule> rules) {
+        this.rules = rules;
+    }
+
+    public boolean isValid(String password) {
+        if (password == null || password.length() < 9) return false;
+
+        for (PasswordRule rule : rules) {
+            if (!rule.validate(password)) {
+                System.out.println("Failed Rule: " + rule.getClass().getSimpleName());
+                return false;
+            }
+        }
+        return true;
+    }
+}
