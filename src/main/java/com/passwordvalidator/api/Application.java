815d7dde53549588aea1dd6fbd9159f1543f629d
paulaallemand <paulaisdev@gmail.com>
2025-01-14 12:03:34 -0300

    Update application file name

--- src/main/java/com/passwordvalidator/api/Application.java
+++ src/main/java/com/passwordvalidator/api/Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PasswordValidatorApiApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(PasswordValidatorApiApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
